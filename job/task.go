f3985874c9d304bbb594d0ba158ccd436630500a
Andrei Avram <avramandrei@ymail.com>
2019-11-15 08:03:18 +0200

    Add docs for Task events.

--- job/task.go
+++ job/task.go
@@ -6,17 +6,19 @@ type Task interface {
 }
 
 // SuccessfulTask is the interface a task must implement to be notified
-// after is executed successfully.
+// after is executed successfully. OnSuccess receives the task result.
 type SuccessfulTask interface {
 	OnSuccess(interface{})
 }
 
 // FailedTask is the interface a task must implement to be notified when fails.
+// OnError receives the returned error.
 type FailedTask interface {
 	OnError(error)
 }
 
 // CancelableTask is the interface a task must implement to be notified when it's canceled.
+// OnCancel receives the error the task was canceled with.
 type CancelableTask interface {
 	OnCancel(error)
 }
